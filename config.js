a82eb28953c0687972616ee14afc4126ad0a1920
Jonathan Gan <jonathang132298@gmail.com>
2024-08-02 19:10:18 -0400

    ??

---
+++ config.js
@@ -0,0 +1,54 @@
+export const SiteData = {
+    // Controls if Site is LIVE
+    isLive: false,
+    notLiveMessage: "WHAT THE FUCK IS THE CA",
+    // Basic
+    name: "WHAT THE F IS THE CA",
+    title: "WHAT THE F IS THE CA",
+    description: "WHAT THE F IS THE CA",
+    url: "liqdwif.io",
+    coinMintAddress: "9UEzwh5TyS2LWFTcoHhbbsb1yMQtJWabECgYWQyopump",
+    image: "https://dd.dexscreener.com/ds-data/tokens/solana/9UEzwh5TyS2LWFTcoHhbbsb1yMQtJWabECgYWQyopump.png?key=62bad9",
+
+    // Useful
+    welcomeMessage: "🚀 WHAT THE F IS THE CA 🚀",
+    raydiumUrl: "https://raydium.io/swap/?inputMint=sol&outputMint=9UEzwh5TyS2LWFTcoHhbbsb1yMQtJWabECgYWQyopump&referrer=9yA9LPCRv8p8V8ZvJVYErrVGWbwqAirotDTQ8evRxE5N",
+    raydiumButtonMessage: "WHAT THE F IS THE CA! 💸",
+
+    // Dex
+    buttonOneTopic: "WHAT THE F IS THE CA",
+    buttonOneMessage: "WHAT THE F IS THE CA 🚀",
+    buttonOneLink: "https://dexscreener.com/solana/5bdhytsrtn2ggjt3a8xyqjs7ctjwzcxpgds7nt3wd9mb",
+
+    // Bonk
+    buttonTwoTopic: "WHAT THE F IS THE CA",
+    buttonTwoMessage: "WHAT THE F IS THE CA 🐶",
+    buttonTwoLink: "https://t.me/bonkbot_bot?start=ref_jyzn2_ca_9UEzwh5TyS2LWFTcoHhbbsb1yMQtJWabECgYWQyopump",
+
+    // SolSniffer
+    buttonThreeTopic: "WHAT THE F IS THE CA",
+    buttonThreeMessage: "WHAT THE F IS THE CA 🐾",
+    buttonThreeLink: "https://solsniffer.com/scanner/9UEzwh5TyS2LWFTcoHhbbsb1yMQtJWabECgYWQyopump",
+
+    // X
+    buttonFourTopic: "WHAT THE F IS THE CA",
+    buttonFourMessage: "WHAT THE F IS THE CA 🐦",
+    buttonFourLink: "https://x.com/LIQDWIF_solana",
+
+    // Features
+    featuresTitle: "WHAT THE F IS THE CA",
+    featuresPoint1: "WHAT THE F IS THE CA",
+    featuresPoint2: "WHAT THE F IS THE CA",
+    featuresPoint3: "WHAT THE F IS THE CA",
+    featuresPoint4: "WHAT THE F IS THE CA",
+    featuresPoint5: "WHAT THE F IS THE CA",
+    featuresPoint6: "WHAT THE F IS THE CA",
+    featuresPoint7: "WHAT THE F IS THE CA",
+    featuresPoint8: "WHAT THE F IS THE CA",
+
+    // Footer
+    footerMessage1: "WHAT THE F IS THE CA 🌐",
+    footerMessage1Link: "https://x.com/LIQDWIF_solana",
+    footerMessage2: "WHAT THE F IS THE CA 🎉",
+    footerMessage2Link: "https://x.com/LIQDWIF_solana",
+};
